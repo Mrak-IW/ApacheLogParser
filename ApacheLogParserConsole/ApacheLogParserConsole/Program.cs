485b58c0e596d66f938000d9c6be9fe518dabed0
Mrak <mrak.iw@gmail.com>
2016-07-23 13:58:21 +0300

    -=[ Initial Commit ]=-

---
+++ ApacheLogParserConsole/ApacheLogParserConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ApacheLogParserConsole
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length == 1 && File.Exists(args[0]))
+			{
+				Console.WriteLine("Файл существует");
+			}
+		}
+	}
+}
